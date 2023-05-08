e541971ad5e4df6c55d5bc142af6ccb9e09764e3
obulamma1231 <132826394+obulamma1231@users.noreply.github.com>
2023-05-09 01:32:02 +0530

    naturalnumber

---
+++ naturalnumber.cpp
@@ -0,0 +1,26 @@
+/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int i=1; 
+	cout<<"\n Enter a number";
+	cin>>n;
+	while(i<=n)  
+	{
+		cout<<i<<"\n";
+		i++;
+	}
+}
